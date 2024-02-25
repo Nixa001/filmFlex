7cbb6e6597dc607c8cf761dfd1458739df790701
nifaye <nicolasfaye31@gmail.com>
2024-02-25 22:11:47 +0000

    display: Add font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "roboto-regular": ["Roboto Regular"],
+        "roboto-medium": ["Roboto Medium"],
+        "roboto-bold": ["Roboto Bold"],
+        "roboto-thin": ["Roboto Thin"],
+        "roboto-black": ["Roboto Black"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
